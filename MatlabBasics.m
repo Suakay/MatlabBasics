98f48e188d4d95e43337f8705e905204c6211f7b
bengü su akay <90608475+Suakay@users.noreply.github.com>
2021-09-18 17:30:55 +0300

    Add files via upload
    initial commit

---
+++ MatlabBasics.m
@@ -0,0 +1,17 @@
+karakter='d';
+okul = 'Erciyes Üniversitesi';
+isim='deniz';
+tamsayi=5;
+ondalikliSayi=3.1416;
+fprintf('Tanimlanan Karakter=%c\n',karakter);
+fprintf('Tanimlanan karakter dizisi=%s/,isim /n');
+fprintf('Tanimlanan tamsayi=%d, tamsayi, tamsayi /n');
+fprintf('Tanimlanan Ondalikli Sayi =%f ,ondaliklisayi/n');
+fprintf('Tanimlanan ondaliklisayi =%f,ondaliklisayi/n');
+
+
+%Did you mean;
+fprintf ('Tanımlı tam sayı =%d\n',tamsayi),
+fprintf('Tanımlı ondalıklı sayı=%g\n',ondalikliSayi),
+fprintf('Tanımlı tam sayı=%g\n',tamsayi);
+fprintf('Okul ismi:%s,tanımlı sayı%g\n',okul,ondalikliSayi)
